1e43b6e87fb924329d94c43f6ca38dc0977643ce
Milicerro <milagros.cerro@radiumrocket.com>
2024-05-21 16:36:33 -0300

    fix spec flow

--- tests/specs/admin.spec.ts
+++ tests/specs/admin.spec.ts
@@ -1,36 +1,44 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 import { LoginPage } from '../pageObjects/login.page';
 import { EmployeePage } from '../pageObjects/employee.page';
 import { LogoutPage } from '../pageObjects/logout.page';
 
-test.describe('Test the new employee creation flow', () => {
-  test('Complete employee creation flow', async ({ page }) => {
-    await test.step('Login admin', async () => {
-      const loginPage = new LoginPage(page);
-      await loginPage.navigate();
-      await loginPage.login('Admin', 'admin123');
-      const dashboardTitle = await loginPage.getDashboardTitle();
-      expect(dashboardTitle).toContain('Dashboard');
-    });
+let page: Page;
 
-    await test.step('Add new employee', async () => {
-      const employeePage = new EmployeePage(page);
-      await employeePage.navigateToPIM();
-      await employeePage.clickAddButton();
-      await employeePage.form('Milagros', 'Cero', '0389');
-    });
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+});
 
-    await test.step('Search for an employee and logout', async () => {
-      const employeePage = new EmployeePage(page);
-      const logoutPage = new LogoutPage(page);
-      await employeePage.employeeBtn();
-      await employeePage.EmpInformation();
-      await employeePage.enterSearchId('0389');
-      await employeePage.searchEmpBtn();
-      await employeePage.foundEmployee();
-      await logoutPage.logoutDropdown();
-      await logoutPage.logoutLink();
-      await expect(page).toHaveURL(/.*login/);
-    });
+test.afterAll(async () => {
+  await page.close();
+});
+
+test.describe.serial('Test the new employee creation flow', () => {
+  test('Complete employee creation flow', async () => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await loginPage.login('Admin', 'admin123');
+    const dashboardTitle = await loginPage.getDashboardTitle();
+    expect(dashboardTitle).toContain('Dashboard');
+  });
+
+  test('Add new employee', async () => {
+    const employeePage = new EmployeePage(page);
+    await employeePage.navigateToPIM();
+    await employeePage.clickAddButton();
+    await employeePage.form('Milagros', 'Cero', '0389');
+  });
+
+  test('Search for an employee and logout', async () => {
+    const employeePage = new EmployeePage(page);
+    const logoutPage = new LogoutPage(page);
+    await employeePage.employeeBtn();
+    await employeePage.EmpInformation();
+    await employeePage.enterSearchId('0389');
+    await employeePage.searchEmpBtn();
+    await employeePage.foundEmployee();
+    await logoutPage.logoutDropdown();
+    await logoutPage.logoutLink();
+    await expect(page).toHaveURL(/.*login/);
   });
 });
